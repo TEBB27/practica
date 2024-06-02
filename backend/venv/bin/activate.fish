1c8620b8b0e7824dcb629cb00f112fd77a16d867
Tomás Esteban Briceño Bernal <110701704+TEBB27@users.noreply.github.com>
2024-06-02 18:17:00 +0000

    borradop del entorno e instalación de uno nuevo

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/user/practica/backend/venv"
+set -gx VIRTUAL_ENV "/workspaces/practica/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
